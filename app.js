b76c0efe1ef0fae982fcf9a046ba17d2ef7f6927
milastycookies <milasty.cookies@gmail.com>
2026-04-15 14:24:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -486,7 +486,7 @@ function renderCharts() {
   const skuMap = {}
   const dailyRevenue = {}
 
-  ordersData.forEach(order => {
+  filteredOrders.forEach(order => {
     const date = new Date(order.created_at)
     const amount = Number(order.total_amount)
 
